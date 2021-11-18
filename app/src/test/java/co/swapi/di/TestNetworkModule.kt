45ce1b89edba0df77d69ccc18e341af65f9eb3a1
Pan <theptai@ekoapp.com>
2021-11-18 16:25:37 +0700

    Update TestNetworkModule.kt

--- app/src/test/java/co/swapi/di/TestNetworkModule.kt
+++ app/src/test/java/co/swapi/di/TestNetworkModule.kt
@@ -1,20 +1,14 @@
 package co.swapi.di
 
 import co.swapi.starships.data.repository.datastore.remote.api.StarshipsApi
-import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 fun testAppModule(baseUrl: String) = module {
-    single<OkHttpClient> {
-        OkHttpClient.Builder()
-            .build()
-    }
     single<Retrofit> {
         Retrofit.Builder()
-            .client(get())
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
