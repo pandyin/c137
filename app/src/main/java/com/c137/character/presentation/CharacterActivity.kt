83897377dc6dd28fa9396ba8eb205449abe2942d
Pan <pandyin@gmail.com>
2022-02-27 16:29:03 +0700

    bind to activity's lifecycle

--- app/src/main/java/com/c137/character/presentation/CharacterActivity.kt
+++ app/src/main/java/com/c137/character/presentation/CharacterActivity.kt
@@ -4,18 +4,19 @@ import android.content.ComponentName
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.c137.character.presentation.service.CharacterServiceBinder
 import com.c137.character.presentation.service.CharacterServiceIntent
 import com.c137.databinding.ActivityCharactersBinding
+import com.trello.rxlifecycle4.components.support.RxAppCompatActivity
+import com.trello.rxlifecycle4.kotlin.bindToLifecycle
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class CharacterActivity : AppCompatActivity() {
+class CharacterActivity : RxAppCompatActivity() {
 
     private val viewModel by viewModel<CharacterViewModel>()
 
@@ -78,6 +79,7 @@ class CharacterActivity : AppCompatActivity() {
 
                     binding.nameTextView.text = character.name
                 }
+                .bindToLifecycle(this)
                 .subscribeOn(Schedulers.io())
                 .subscribe()
         }
