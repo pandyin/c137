bcea43617d5d544b8b77f13d26889b9dc1c2fab9
Pan <pandyin@gmail.com>
2022-03-04 11:37:58 +0700

    Create FakeClass.kt

---
+++ app/src/main/java/com/c137/character/presentation/FakeClass.kt
@@ -0,0 +1,47 @@
+package com.c137.character.presentation
+
+import android.view.View
+import java.util.*
+
+class FakeClass {
+
+    // Object Expression (anonymous object)
+    private val foo = object {
+        // Is best for one-time-used.
+        // Eager initialization by default.
+
+        private val greetingText = "HELLO_WORLD"
+
+        fun callMe(): String {
+            return HELLO_WORLD
+        }
+    }
+
+    private val callBack = object : View.OnClickListener {
+        override fun onClick(p0: View?) {
+            TODO("Not yet implemented")
+        }
+    }
+
+    // Object Declaration
+    object Hi {
+        //Singleton by default.
+        //Lazy initialization by default.
+
+        private val greetingText: String by lazy {
+            UUID.randomUUID().toString()
+        }
+
+        fun callMe(): String = greetingText
+    }
+
+    companion object /*NAME*/ {
+        // Initialized when the class is loaded.
+
+        const val HELLO_WORLD = "HELLO_WORLD"
+
+        fun callMe(): String {
+            return HELLO_WORLD
+        }
+    }
+}
