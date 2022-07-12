ccc18c47ff04d4b4748ec1f914240c6374ea72cd
Pan <pandyin@gmail.com>
2022-07-12 09:33:54 +0200

    renamed Character table

--- app/src/main/java/com/c137/data/model/CharacterData.kt
+++ app/src/main/java/com/c137/data/model/CharacterData.kt
@@ -3,7 +3,7 @@ package com.c137.data.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "character")
 data class CharacterData(
     @PrimaryKey val characterId: Int,
     val name: String,
