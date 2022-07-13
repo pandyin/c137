d3c5ff99e5e06444bb00b362120ae3889dc4fb04
Pan <pandyin@gmail.com>
2022-07-13 11:46:55 +0200

    enable aggregating task

--- app/src/main/java/com/c137/presentation/MainActivity.kt
+++ app/src/main/java/com/c137/presentation/MainActivity.kt
@@ -2,14 +2,12 @@ package com.c137.presentation
 
 import android.content.ServiceConnection
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
 import com.c137.R
 import com.c137.databinding.ActivityMainBinding
 import com.c137.feature.search.MainViewModel
 import com.c137.feature.search.Response
-import com.google.gson.Gson
 import com.trello.rxlifecycle4.components.support.RxAppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -46,7 +44,9 @@ class MainActivity : RxAppCompatActivity() {
                         val errorMessage = it.errorMessage ?: getString(R.string.unknown_error)
                         Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
                     }
-                    false -> Log.e("testtest", Gson().toJson(it))
+                    false -> {
+
+                    }
                 }
             }.subscribe()
     }
