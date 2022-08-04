a2991f4451e309fdd8cf9e7185a052a44a979ed2
Pan <pandyin@gmail.com>
2022-08-04 15:10:56 +0200

    added a kmp module

---
+++ kmp-episode/src/commonMain/kotlin/com/c137/kmp/episode/Greeting.kt
@@ -0,0 +1,7 @@
+package com.c137.kmp.episode
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ kmp-episode/src/commonMain/kotlin/com/c137/kmp/episode/Platform.kt
@@ -0,0 +1,5 @@
+package com.c137.kmp.episode
+
+expect class Platform() {
+    val platform: String
+}
